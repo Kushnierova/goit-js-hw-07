aa77a58dee1edd26a2175e06bf49c7481630c481
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-09 16:06:20 +0200

    16:06

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,8 +10,8 @@ function renderList() {
     .map(
       ({ preview, original, description }) =>
         `<li class="gallery__item">
-        <a class="gallery__link" href="${original}"> 
-        <img 
+        <a class="gallery__link" href="${original}">
+        <img
         class="gallery__image"
         src="${preview}"
         data-source="${original}"
@@ -31,11 +31,10 @@ gallery.addEventListener("click", (e) => {
     return;
   }
 });
-new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionPosition: 'bottom',
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
     captionDelay: 250,
   });
-  
+
   console.log(galleryItems);
